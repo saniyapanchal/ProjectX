aea13365ef83e04f2ce62c8e30655a7a471e30a3
saniyapanchal <154547669+saniyapanchal@users.noreply.github.com>
2023-12-22 20:17:42 +0530

    tables

---
+++ db.sql
@@ -0,0 +1,33 @@
+create table bank1 ( name varchar(50), amount int(15), branch varchar(50));
+insert into bank1 values ('Joy', 50000 , 'BOI');
+insert into bank1 values ('Dax', 50000 , 'IOB');
+insert into bank1 values ('Rashida', 30000 , 'SBI');
+insert into bank1 values ('Helly', 20000 , 'HDFC');
+insert into bank1 values ('Saniya', 20000 , 'IOB');
+
+create table student1 (name varchar(50), Rollno int(15), state varchar(50));
+insert into student1 values ('Joy', 120, 'Mumbai');
+insert into student1 values ('Dax', 118, 'Mumbai');
+insert into student1 values ('Rashida', 120, 'Delhi');
+insert into student1 values ('Helly', 211, 'Ahmedabad');
+insert into student1 values ('Saniya', 200, 'Ahmedabad');
+
+create table shopping1 ( C_name varchar(50), Buy varchar(50), Shop varchar(50));
+insert into shopping1 values ('Joy', 'Food' , 'Mumbai');
+insert into shopping1 values ('Dax', 'clothes', 'Mumbai');
+insert into shopping1 values ('Rashida', 'makeup', 'Delhi');
+insert into shopping1 values ('Helly', 'notebook', 'Ahmedabad');
+insert into shopping1 values ('Saniya', 'Food', 'Ahmedabad');
+
+create table myfamily1 ( name varchar(50), profession varchar(50), hobby varchar(50), realtion varchar(50));
+insert into myfamily1 values ('Rakesh', 'Accounting' , 'Travelling' , 'Father');
+insert into myfamily1 values ('Aarti', 'Housewife', 'Sports' , 'Mother');
+insert into myfamily1 values ('Rushil', 'Student', 'Studing' , 'Brother');
+insert into myfamily1 values ('Saniya', 'Student', 'Gaming' , 'me');
+
+create table books1 ( name varchar(50), price int(50), sell int(500));
+insert into books1 values ('Textbook', 40, 100);
+insert into books1 values ('Singleline', 20 , 200);
+insert into books1 values ('Maths', 25 , 150);
+insert into books1 values ('Drawing', 35, 50);
+insert into books1 values ('Diary', 45 , 20);
